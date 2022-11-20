19a9127fc43de0e38f10b61c6e054e71a1438aad
jeradrutnam <jeradrutnam@outlook.com>
2022-11-20 12:32:19 +0530

    Spread vercel.json

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -23,8 +23,9 @@
  **/
 
 import axios from "axios";
+import deploymentConfig from "../deployment.config.json";
 
-const url = "http://localhost:5000/posts";
+const url = deploymentConfig.API_ENDPOINT || "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
