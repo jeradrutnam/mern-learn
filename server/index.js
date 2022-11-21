002cda42a154713b3835ad641e068ae22560c295
jeradrutnam <jeradrutnam@outlook.com>
2022-11-21 07:23:15 +0530

    Re-enable body-parser in server

--- server/index.js
+++ server/index.js
@@ -35,17 +35,18 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(express.json())
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
     res.send("MERN learn project API.");
 });
 
-mongoose.connect(process.env.CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => { console.error(error.message) });
+// mongoose.connect(process.env.CONNECTION_URL)
+//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+//     .catch((error) => { console.error(error.message) });
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
