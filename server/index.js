2a69217251d7e47e156991bc8052e9f664489c50
jeradrutnam <jeradrutnam@outlook.com>
2022-11-21 07:17:24 +0530

    Change server for vercel test

--- server/index.js
+++ server/index.js
@@ -30,21 +30,24 @@ import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use(express.json())
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
     res.send("MERN learn project API.");
 });
 
-const PORT = process.env.PORT || 5000;
+// mongoose.connect(process.env.CONNECTION_URL)
+//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+//     .catch((error) => { console.error(error.message) });
 
-mongoose.connect(process.env.CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => { console.error(error.message) });
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
